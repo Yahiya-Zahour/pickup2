243f01d4a277446da032c2fd601230f246be1d3a
Yahya-Zahour <76777699+Yahya-Zahour@users.noreply.github.com>
2021-02-04 15:41:35 +0530

    practice

--- src/Component/ClassComponent/ClassChild.jsx
+++ src/Component/ClassComponent/ClassChild.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 
 export class ClassChild extends Component {
+   
+
     render() {
         return (
             <div>
+                
                 <h1>my name is {this.props.name} i am {this.props.age} year old!!!</h1>
             </div>
         );
--- src/Component/ClassComponent/ClassParent.jsx
+++ src/Component/ClassComponent/ClassParent.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import  {ClassChild}  from "./ClassChild";
 export class ClassParent extends Component {
+    state={
+        person:[
+            {name:"thor"},
+            {name:"black panther" ,age:18},
+            {name:"monster" ,age:29},
+        ],
+        };
     render() {
         return (
             <div>
                 <ClassChild name="zahoor" age="20"/>
                 <ClassChild name="yahiya" age="17"/>
                 <ClassChild name="muhammad"age="19"/>
+                <ClassChild name={this.state.person[2].name} age={this.state.person[2].age}/>
             </div>
         );
     }
