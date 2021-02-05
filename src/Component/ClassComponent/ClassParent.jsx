f3370afadc87a91721ebafdeae971545ff7ba9d4
Yahya-Zahour <76777699+Yahya-Zahour@users.noreply.github.com>
2021-02-05 16:16:56 +0530

    implemented work

--- src/Component/ClassComponent/ClassParent.jsx
+++ src/Component/ClassComponent/ClassParent.jsx
@@ -1,28 +1,39 @@
 import React, { Component } from 'react';
 import  {ClassChild}  from "./ClassChild";
 export class ClassParent extends Component {
+    
     state={
         person:[
-            {name:"thor"},
+            {name:"thor" ,age:35},
             {name:"black panther" ,age:18},
             {name:"monster" ,age:29},
+            {name:"thor" ,age:35},
         ],
         };
+                  
+
+        changeNameHandler = ()=> {
+            this.setState({
+                person:[
+                    {name:"Bruce", age:26},
+                    {name:"clark", age:26},
+                    {name:"Bruce", age:26},
+                    {name:"clark", age:26},
+
+                ],
+             });
+         };
     render() {
         return (
             <div>
-                <ClassChild name="zahoor" age="20"/>
-                <ClassChild name="yahiya" age="17"/>
-                <ClassChild name="muhammad"age="19"/>
-                <ClassChild name={this.state.person[2].name} age={this.state.person[2].age}/>
-                <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
-            <button>login</button>
+                <button onClick={this.changeNameHandler}>On Click Event</button>
+                <ClassChild name={this.state.person[0].name} age={this.state.person[0].age}/>
+                <ClassChild name={this.state.person[1].name} age={this.state.person[1].age}/>
+                <ClassChild name={this.state.person[2].name}age={this.state.person[2].age}/>
+                <ClassChild name={this.state.person[3].name} age={this.state.person[3].age}/>
+           
+         
+
             </div>
         );
     }
