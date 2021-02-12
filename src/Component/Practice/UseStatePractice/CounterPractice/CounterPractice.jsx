34d5e0c9e82170659617775f3a2687f4d7c09054
Yahya-Zahour <76777699+Yahya-Zahour@users.noreply.github.com>
2021-02-12 15:13:51 +0530

    work

--- src/Component/Practice/UseStatePractice/CounterPractice/CounterPractice.jsx
+++ src/Component/Practice/UseStatePractice/CounterPractice/CounterPractice.jsx
@@ -16,6 +16,13 @@ import React,{useState} from 'react';
             <button  onClick={handleCounter}>Add</button>
             <h1>Decrease Number...</h1>
             <button onClick={handleDeleteCounter}>Delete</button>
+            <h1>Increase Number...</h1><button>{addNumber}</button>
+            <br/>
+            <br/>
+            <button  onClick={handleCounter}>Add</button>
+            <h1>Decrease Number...</h1>
+            <button onClick={handleDeleteCounter}>Delete</button>
+            
         </div>
     );
 };
