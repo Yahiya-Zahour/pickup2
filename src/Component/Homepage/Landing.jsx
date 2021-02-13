e83c2b4ab27b621e27f74f99cb21dbb7508ff1ac
Yahya-Zahour <76777699+Yahya-Zahour@users.noreply.github.com>
2021-02-13 15:57:26 +0530

    work

--- src/Component/Homepage/Landing.jsx
+++ src/Component/Homepage/Landing.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {MainHeader} from "../HeaderSection/MainBodyHeader/MainHeader";
 import { ClassParent } from '../ClassComponent/ClassParent';
 import CounterPractice from '../Practice/UseStatePractice/CounterPractice/CounterPractice';
+import { FetchingDataFromJsonChild } from '../Practices/FetchingDataFromJson/FetchingDataFromJsonChild';
+import { FetchingDataFromJsonParent } from '../Practices/FetchingDataFromJson/FetchingDataFromJsonParent';
 // import {ClassComponent} from './ClassPairent';
 // import {ClassPairent} from "./ClassComponent/ClassPairent";
 
@@ -11,9 +13,11 @@ import CounterPractice from '../Practice/UseStatePractice/CounterPractice/Counte
     <div>
           {/* <ClassPairent /> */}
           {/* <ClassParent/> */}
-          <CounterPractice/>
-          <CounterPractice.css/>
+          {/* <CounterPractice/> */}
+          {/* <CounterPractice.css/> */}
           {/* <MainHeader/> */}
+          <FetchingDataFromJsonChild/>
+          <FetchingDataFromJsonParent/>
             
         </div>
         );
