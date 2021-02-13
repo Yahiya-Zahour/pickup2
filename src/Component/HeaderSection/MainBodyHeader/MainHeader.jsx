1a128523f56e3d184e4e8f44e84e5928630bd1e3
Yahya-Zahour <76777699+Yahya-Zahour@users.noreply.github.com>
2021-02-13 14:24:58 +0530

    work

--- src/Component/HeaderSection/MainBodyHeader/MainHeader.jsx
+++ src/Component/HeaderSection/MainBodyHeader/MainHeader.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
-import {Nav,FormControl,Button,Navbar,Form,}  from 'react-bootstrap';
+import {Card,CardImg,ListGroup,ListGroupItem,}  from 'react-bootstrap';
 import "./MainHeader.css";
 export const MainHeader = () => {
     return (
         <div>
 
-  <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-    </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-info">Search</Button>
-    </Form>
-  </Navbar>
+<Card className="fk" style={{ width: '27rem' }}>
+  <Card.Img width="400px"  src="Assets/Pickupbiz.jpg" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content. Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga placeat minima consectetur officiis, nam eligendi voluptate explicabo tempora aliquam eveniet?
+    </Card.Text>
+  </Card.Body>
+  <ListGroup className="list-group-flush">
+    <ListGroupItem>Cras justo odio</ListGroupItem>
+    <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+    <ListGroupItem>Vestibulum at eros</ListGroupItem>
+  </ListGroup>
+  <Card.Body>
+    <Card.Link href="#">Card Link</Card.Link>
+    <Card.Link href="#">if you are bad i m your dad</Card.Link>
+  </Card.Body>
+</Card>
   <br />
 
         </div>
