4c4ce6a14bf031a52fc0be3fa0a6aec5b550bc6c
Yahya-Zahour <76777699+Yahya-Zahour@users.noreply.github.com>
2021-02-13 16:08:03 +0530

    work

--- src/Component/Practices/FetchingDataFromJson/FetchingDataFromJsonParent.jsx
+++ src/Component/Practices/FetchingDataFromJson/FetchingDataFromJsonParent.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import {FetchingDataFromJsonChild} from "../FetchingDataFromJson/FetchingDataFromJsonChild";
 export const FetchingDataFromJsonParent = () => {
     return (
