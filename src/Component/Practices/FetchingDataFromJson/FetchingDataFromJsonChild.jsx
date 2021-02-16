baf1c16b5a0faec54630fc5018c1359d116ae305
Yahya-Zahour <76777699+Yahya-Zahour@users.noreply.github.com>
2021-02-16 16:21:33 +0530

    work

--- src/Component/Practices/FetchingDataFromJson/FetchingDataFromJsonChild.jsx
+++ src/Component/Practices/FetchingDataFromJson/FetchingDataFromJsonChild.jsx
@@ -14,6 +14,7 @@ import {Card,Button} from "react-bootstrap"
     </Card.Text>
     <Button variant="primary">Go somewhere</Button>
     <Button variant="primary">Go somewhere</Button>
+    <Button variant="primary">Go somewhere</Button>
     
   </Card.Body>
 </Card>
