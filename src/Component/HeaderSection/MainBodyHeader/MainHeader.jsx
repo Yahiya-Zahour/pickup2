ba384897fb71f0a677103b1ae0210603ee3e7071
Yahya-Zahour <76777699+Yahya-Zahour@users.noreply.github.com>
2021-03-07 14:29:27 +0530

    redux

--- src/Component/HeaderSection/MainBodyHeader/MainHeader.jsx
+++ src/Component/HeaderSection/MainBodyHeader/MainHeader.jsx
@@ -25,10 +25,7 @@ export const MainHeader = () => {
   </Card.Body>
 </Card>
 <button>submit</button>
-<button>submit</button>
-<button>submit</button>
-<button>submit</button>
-<button>submit</button>
+
 
 
         </div>
